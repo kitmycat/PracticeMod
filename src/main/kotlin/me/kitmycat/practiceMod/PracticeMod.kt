9435667c2f541e8452149a0a0f02e52ae2e2065c
kitmycat <darkstrikebussiness@gmail.com>
2025-01-08 01:43:14 -0600

    Created mod

---
+++ src/main/kotlin/me/kitmycat/practiceMod/PracticeMod.kt
@@ -0,0 +1,14 @@
+package me.kitmycat.practiceMod
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PracticeMod : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
